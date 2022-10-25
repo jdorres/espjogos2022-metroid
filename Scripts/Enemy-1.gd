6429924f337af49c3cd3847686508f16d669c533
rafaelmfiles <rafael_mfiles@hotmail.com>
2022-10-25 18:41:41 -0300

    adicionado Morph Ball e atualização do inimigo

--- Scripts/Enemy-1.gd
+++ Scripts/Enemy-1.gd
@@ -2,17 +2,42 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 var speed = 40
+var movement_direction = 'left'
+
 export var direction = Vector2.LEFT
+onready var right_collision = $Right_Collision
+onready var left_collision = $Left_Collision
+onready var right_floor = $Right_Floor_Collision
+onready var left_floor = $Left_Floor_Collision
 
 func _process(delta):
 	velocity = speed * direction
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if not right_floor.is_colliding() and movement_direction == 'right':
+		direction = Vector2.DOWN
+		rotation_degrees = 90
+
+	if not left_floor.is_colliding() and movement_direction == 'left':
+		direction = Vector2.DOWN
+		rotation_degrees = 270
+		
+	if left_collision.is_colliding() and movement_direction == 'right':
+		print('left')
+		direction = Vector2.RIGHT
+		rotation_degrees = 0
+		
+	if right_collision.is_colliding() and movement_direction == 'left':
+		direction = Vector2.LEFT
+		rotation_degrees = 0
+		print('right')
+		
 
 func _on_FloorChecker_body_exited(body):
-	direction = Vector2.DOWN
-	rotation_degrees = -90
-
-func _on_WallChecker_body_entered(body):
-	print('collision wall')
-	#direction = Vector2.UP
+	#direction = Vector2.DOWN
+	#rotation_degrees = -90
+	pass
 	
+func _on_WallChecker_body_entered(body):
+	pass
+	#direction = Vector2.UP
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,8 +7,10 @@ export var gravity = 2500
 export var jump_speed = 1000
 onready var player := $Sprite
 var player_dir = 1 # 0 - left, 1 - right
+var player_form = 'standing'
 var bullet_dist_left = Vector2(60, -20)
 var bullet_dist_right = Vector2(-60, -20)
+var teste = 1
 
 export (PackedScene) var box : PackedScene
 
@@ -20,54 +22,99 @@ func _physics_process(delta):
 	get_action_buttons()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
 
 func get_8way_input():
 	
-	if(is_on_floor()):
-		velocity.x = Input.get_action_strength("right")-Input.get_action_strength("left")
-		var velocity_2 = velocity.normalized() * speed
-		velocity.x =velocity_2.x
-		if velocity.x < 0:
-			player_dir = 0
-			player.flip_h = true
-			player.play("walk")
-		elif velocity.x > 0:
-			player_dir = 1
-			player.flip_h = false
-			player.play("walk")
-		elif velocity.y > 0:
-			print("down")
-		elif velocity.y < 0:
-			print("up")
-		else:
-			player.frame = 0
+	if player_form == 'standing':
+		if(is_on_floor()):
+			velocity.x = Input.get_action_strength("right")-Input.get_action_strength("left")
+			var velocity_2 = velocity.normalized() * speed
+			velocity.x =velocity_2.x
+			if velocity.x < 0:
+				player_dir = 0
+				player.flip_h = true
+				player.play("walk")
+			elif velocity.x > 0:
+				player_dir = 1
+				player.flip_h = false
+				player.play("walk")
+			elif velocity.y > 0:
+				print("down")
+			elif velocity.y < 0:
+				print("up")
+			else:
+				player.frame = 0
+				if(is_on_floor()):
+					if(player_dir == 0):
+						player.flip_h = true
+						player.play("idle")
+					else:
+						player.flip_h = false
+						player.play("idle")
+				player.stop()
+				
+	if player_form == 'ball':
+		if(is_on_floor()):
+			velocity.x = Input.get_action_strength("right")-Input.get_action_strength("left")
+			var velocity_2 = velocity.normalized() * speed
+			velocity.x =velocity_2.x
+			if velocity.x < 0:
+				player_dir = 0
+				player.flip_h = true
+				player.play("ball_movement")
+			elif velocity.x > 0:
+				player_dir = 1
+				player.flip_h = false
+				player.play("ball_movement")
+			elif velocity.y > 0:
+				print("down")
+			elif velocity.y < 0:
+				print("up")
+			else:
+				player.frame = 0
+				if(is_on_floor()):
+					if(player_dir == 0):
+						player.flip_h = true
+						player.play("ball_idle")
+					else:
+						player.flip_h = false
+						player.play("ball_idle")
+				player.stop()
+			
+func get_action_buttons():
+	if player_form == 'standing':
+		var jump_ajust: float = jump_speed
+		if Input.is_action_pressed("jump") and is_on_floor():
+			velocity.y = -jump_ajust
 			if(is_on_floor()):
 				if(player_dir == 0):
 					player.flip_h = true
-					player.play("idle")
+					player.play("jump")
 				else:
 					player.flip_h = false
-					player.play("idle")
-			player.stop()
-		
-func get_action_buttons():
-	var jump_ajust: float = jump_speed
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = -jump_ajust
-		if(is_on_floor()):
-			if(player_dir == 0):
-				player.flip_h = true
-				player.play("jump")
+					player.play("jump")
+		if Input.is_action_just_pressed("shoot"):
+			var bullet_dist = 0
+			if(player_dir == 1):
+				bullet_dist = bullet_dist_left
 			else:
-				player.flip_h = false
-				player.play("jump")
-	if Input.is_action_just_pressed("shoot"):
-		var bullet_dist = 0
-		if(player_dir == 1):
-			bullet_dist = bullet_dist_left
-		else:
-			bullet_dist = bullet_dist_right
-		var b := box.instance()
-		b.setDirection(player_dir)
-		b.position = global_position + bullet_dist
-		owner.add_child(b)
+				bullet_dist = bullet_dist_right
+			var b := box.instance()
+			b.setDirection(player_dir)
+			b.position = global_position + bullet_dist
+			owner.add_child(b)
+		
+		if Input.is_action_just_pressed("down"):
+			$CollisionShape2D.scale.y = 0.4
+			$CollisionShape2D.scale.x = 0.8
+			player.play("ball_idle")
+			player_form = 'ball'
+	
+	if player_form == 'ball':
+		if Input.is_action_just_pressed("up"):
+			$CollisionShape2D.scale.y = 1
+			$CollisionShape2D.scale.x = 1
+			player_form = 'standing'
+		
+
