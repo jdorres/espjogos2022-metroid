4cd0cd0da1f5030eee85eea608ed520904142dee
rafaelmfiles <rafael_mfiles@hotmail.com>
2022-11-25 21:53:59 -0300

    Ajuste de camera

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -1,20 +1,8 @@
 extends Camera2D
 
 
-export var nodePlayer: NodePath
-var player
-var stage1Limit = Vector2(330, 1110)
-var cameraLimit= Vector2() 
-var x_camera_limits=100;
-func _ready():
-	player = get_node(nodePlayer);
+onready var player = get_node("../Player")
 
 func _process(delta): 
-	if(player.global_position.x >= (global_position.x+x_camera_limits) or player.global_position.x <=  (global_position.x-x_camera_limits)):
-		if(global_position.x < player.global_position.x ):
-			global_position.x+=player.speed*delta;
-		elif(global_position.x > player.global_position.x ):
-			global_position.x-=player.speed*delta;
-		else:
-			global_position.x+=0
-
+	position.x = player.position.x
+	position.y = player.position.y
